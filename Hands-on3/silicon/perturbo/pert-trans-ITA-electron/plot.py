c5a2c8f1d4fd9e2492e9cbe409d10a445e472b61
Dhruv Desai <ddesai@caltech.edu>
2023-09-16 16:07:37 -0700

    Some additional changes

--- Hands-on3/silicon/perturbo/pert-trans-ITA-electron/plot.py
+++ Hands-on3/silicon/perturbo/pert-trans-ITA-electron/plot.py
@@ -2,24 +2,25 @@ import perturbopy.postproc as ppy
 import matplotlib.pyplot as plt
 import numpy as np
 
+si_trans_rta = ppy.Trans.from_yaml('../pert-trans-RTA-electron/si_trans-rta.yml')
 si_trans_ita = ppy.Trans.from_yaml('si_trans-ita.yml')
 fig, ax  = plt.subplots()
 
 # Optional
 plt.rcParams.update(ppy.plot_tools.plotparams)
 
-temperatures = np.array(list(si_trans_ita.temper.values()))
+temperatures_rta = np.array(list(si_trans_rta.temper.values()))
+temperatures_ita = np.array(list(si_trans_ita.temper.values()))
 
 # Get xx components of mobilities
-mobilities = np.array(list(si_trans_ita.mob.values()))[:, 0, 0]
+mobilities_rta = np.array(list(si_trans_rta.mob.values()))[:, 0, 0]
+mobilities_ita = np.array(list(si_trans_ita.mob.values()))[:, 0, 0]
 
-ax.plot(temperatures, mobilities,marker='o')
-
-ax.set_xlabel(f"Temperature ({si_trans_ita.temper.units})")
-ax.set_ylabel(f"Mobility ({si_trans_ita.mob.units})")
-
-plt.show()
-plt.savefig('si-trans-ita.png')
+ax.plot(temperatures_rta, mobilities_rta,marker='o',color='b',label='RTA')
+ax.plot(temperatures_ita, mobilities_ita,marker='o',color='r',label='ITA')
 
 
-
+ax.set_xlabel(f"Temperature ({si_trans_rta.temper.units})")
+ax.set_ylabel(f"Mobility ({si_trans_rta.mob.units})")
+ax.legend(fontsize=14)
+plt.savefig('si-trans.png')
