51e85496a8b32cf588415b7e6e4b7af8aff10f15
imsorensen <isorense@caltech.edu>
2023-09-18 07:27:12 -0400

    Commented scripts, updated velocity field curve plot, changed dynamics-pp to single run, added README to qe2pert so directory gets stored in github

--- Hands-on4/perturbo/dynamics-pp/plot_velocity_field_curve.py
+++ Hands-on4/perturbo/dynamics-pp/plot_velocity_field_curve.py
@@ -3,14 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Parameters
 prefix = 'gaas'
-efields = np.arange(0, 3000, 200)
+efields = np.arange(0, 1600, 200)
 nsnaps_per_run = 20
 
+# Store drift velocities at the end
+# of each run
 drift_vels = []
 
 # yml file
-yml_file = f'efield-3000/{prefix}_dynamics-pp.yml'
+yml_file = f'{prefix}_dynamics-pp.yml'
 
 # Opening yml file
 with open(yml_file, 'r') as stream:
@@ -21,17 +24,24 @@ with open(yml_file, 'r') as stream:
     except yaml.YAMLError as e:
         print(e)
 
+# Get velocities from dictionary
 vels_efield = dct['dynamics-pp']['velocity']
 
+# Loop over field strengths
 for i, efield in enumerate(efields):
     if i == 0:
+        # Hard code to 0 as not outputted
+        # when E = 0
         vel_i = 0.0
     else:
+        # Get drift velocity of final snap in run
         idx = i * nsnaps_per_run + (nsnaps_per_run - 1)
         vel_i = (-1.0) * vels_efield[idx]
 
+    # Store in list
     drift_vels.append(vel_i)
 
+# Rescale for plotting purposes
 drift_vels = np.asarray(drift_vels) / 1e6
 
 # Plot velocity-field curve
@@ -40,4 +50,4 @@ ax.plot((efields / 1000), drift_vels, ls='', marker='o')
 ax.set_xlabel('E (kV/cm)')
 ax.set_ylabel('$v_d$ ($10^6$ cm/s)')
 plt.savefig('velocity-field_curve.png')
-plt.show()
+# plt.show()
--- Hands-on4/perturbo/dynamics-run/run.sh
+++ Hands-on4/perturbo/dynamics-run/run.sh
@@ -2,56 +2,47 @@
 
 PREFIX='gaas'
 
-# Test with two values first, then run full selection
-# EFIELDS=($(seq 200 200 400))
-# EFIELDS=($(seq 200 200 3000))
+# List of electric field strengths
 # EFIELDS=(0)
-EFIELDS=($(seq 0 200 3000))
+EFIELDS=($(seq 200 200 1600))
 
-# OpenMP variables
+# OpenMP variable
 OMP_THREADS=4
-
-# Commands change slightly based on OS
-# OS='MACOS'
-OS='LINUX'
 
 for efield in ${EFIELDS[@]}
 do
    echo dynamics-run for Efield= $efield
 
+   # Create directory efield-${field strength number}
    DIR=efield-$efield
    mkdir -p $DIR
 
+   # Move into directory
    cd $DIR
 
-   #change pert.in
+   # Copy pert-ref.in into directory as pert.in
    cp ../pert-ref.in  ./pert.in
-   if [ "$OS" == "MACOS" ]; then
-      sed -i '' "s|.*boltz_efield(1).*| boltz_efield(1)      = $efield.0|g"   pert.in
-      if [ $efield != 0 ]; then
-         sed -i '' "s|.*boltz_init_dist.*| boltz_init_dist      = 'restart'|g"   pert.in
-         sed -i '' "s|.*load_scatter_eph.*| load_scatter_eph      = .true.|g"   pert.in
-      fi
-   elif [ "$OS" == "LINUX" ]; then
-      sed -i "s|.*boltz_efield(1).*| boltz_efield(1)      = $efield.0|g"   pert.in
 
-      if [ $efield != 0 ]; then
-         sed -i "s|.*boltz_init_dist.*| boltz_init_dist      = 'restart'|g"   pert.in
-         sed -i "s|.*load_scatter_eph.*| load_scatter_eph      = .true.|g"   pert.in
-      fi
-   else
-      echo OS not supported
+   # Change pert.in file
+   # Change efield(1) parameter
+   sed -i "s|.*boltz_efield(1).*| boltz_efield(1)      = $efield.0|g"   pert.in
+
+   # If E != 0, set calculation to restart
+   # and read e-ph matrix elements from file
+   if [ $efield != 0 ]; then
+      sed -i "s|.*boltz_init_dist.*| boltz_init_dist      = 'restart'|g"   pert.in
+      sed -i "s|.*load_scatter_eph.*| load_scatter_eph      = .true.|g"   pert.in
    fi
 
    # link prefix_epr.h5 and prefix_tet.h5
-      ln -sf ../../../qe2pert/${PREFIX}_epr.h5
-      ln -sf ../../setup/${PREFIX}_tet.h5
+   ln -sf ../../../qe2pert/${PREFIX}_epr.h5
+   ln -sf ../../setup/${PREFIX}_tet.h5
 
    # copy prefix.temper
    cp ../../setup/${PREFIX}.temper .
 
-   # If restarting, need to link cdyna file
-   # and tmp directory
+   # If E != 0, link cdyna file for restart
+   # and tmp file for e-ph matrix elements
    if [ $efield != 0 ]; then
       echo Linking cdyna and tmp
       ln -sf ../efield-0/${PREFIX}_cdyna.h5
@@ -64,9 +55,9 @@ do
 
    echo Done $efield
 
-   # wait for a short period of time.  
-   #done, return to upper directory.   
+   # Return to upper directory
    cd ..
- 
+   
+   # Wait for a short amount of time 
    sleep 1
 done
