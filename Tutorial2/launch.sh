756dbc0d1c493651c94cb26b746fb165956816d2
yaoluo <49070669+yaoluo@users.noreply.github.com>
2023-09-20 20:50:22 -0700

    Add files via upload

---
+++ Tutorial2/launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+
+
+docker run -v /Users/yaoluo/workshop/perturbo-workshop-2023-main/Tutorial2:/home/user/run/epr_gen --user 500 -it --rm --name perturbo perturbo/perturbo:gcc_openmp
+
